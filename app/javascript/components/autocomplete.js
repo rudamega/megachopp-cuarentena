8c8fd0247caf10e2bb177a717cc34582f88bca28
ruben medina <ruben@megachopp.com.py>
2020-09-12 12:44:31 -0400

    ..

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -19,7 +19,7 @@ const autocomplete = () => {
 if (document.getElementById('select2')) {
 const id = document.getElementById('select2').value;
 if (id != "") {
-fetch(`http://localhost:3000/api/v1/clientes/${id}`)
+fetch(`http://megachopp.com.py/api/v1/clientes/${id}`)
   .then(response => response.json())
   .then((data) => {
     const cliente = document.getElementById('cliente');
